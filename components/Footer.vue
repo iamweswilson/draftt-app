b6053231a6edb571e20bfb930335a7676be5af3c
Wes Wilson <wes@iamweswilson.com>
2020-12-08 13:24:16 -0800

    fix for footer link spacing
    missing the stupid <p> tags. I still don't get it.

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,11 +2,17 @@
   <footer
     class="container flex justify-center mt-10 py-12 text-sm text-gray-800 b-t mx-auto"
   >
-    Built with
-    <a href="https://nuxtjs.org" target="_blank">NuxtJS</a> and
-    <a href="https://tailwindcss.com/" target="_blank">Tailwind</a>. Deployed to
-    <a href="https://netlify.com">Netlify</a>.
+    <p>
+      Built with &#x2665; using
+      <a href="https://nuxtjs.org" target="_blank">NuxtJS</a> and
+      <a href="https://tailwindcss.com/" target="_blank">Tailwind</a>. Deployed
+      to <a href="https://netlify.com" target="_blank">Netlify</a>.
+    </p>
   </footer>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+a {
+  text-decoration: underline;
+}
+</style>
