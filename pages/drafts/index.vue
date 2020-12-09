e837150bcd8a8fbea35dc5c01004883cbbaa261c
Wes Wilson <wes@iamweswilson.com>
2020-12-09 14:45:48 -0800

    draft listing page concept

--- pages/drafts/index.vue
+++ pages/drafts/index.vue
@@ -1,20 +1,178 @@
 <template>
   <div>
     <h1 class="text-4xl text-blue-900 font-bold">Browse drafts</h1>
-    <ul>
-      <li>
-        <NuxtLink to="/drafts/single">Individual draft</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/drafts/single">Individual draft</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/drafts/single">Individual draft</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/drafts/single">Individual draft</NuxtLink>
-      </li>
-    </ul>
+    <div
+      class="grid w-full grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4"
+    >
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1584804738473-a49b7441c464?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1553996078-07277bda2756?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2538&q=80"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1607443359781-1f90813dd803?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1579762715118-a6f1d4b934f1?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aWxsdXN0cmF0aW9ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1580196969807-cc6de06c05be?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8aWxsdXN0cmF0aW9ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1521911528923-9c3838123490?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8aWxsdXN0cmF0aW9ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1541692033570-5b2e9c4e2aa9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8aWxsdXN0cmF0aW9ufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1521633285489-5ad960964604?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fGlsbHVzdHJhdGlvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1575550597909-fff46d723735?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fGlsbHVzdHJhdGlvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1576497588294-94553e04534e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fGlsbHVzdHJhdGlvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1578406843598-34a4518d168d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzB8fGlsbHVzdHJhdGlvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1580130601254-05fa235abeab?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDV8fGlsbHVzdHJhdGlvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1580130732455-6663b4ce9f6e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NDZ8fGlsbHVzdHJhdGlvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+      <NuxtLink
+        to="/drafts/single"
+        class="block bg-white rounded h-full text-grey-darkest no-underline shadow-md draft"
+      >
+        <figure class="min-h-full min-w-full relative overflow-hidden">
+          <img
+            class="absolute h-full w-full object-cover object-center rounded"
+            src="https://images.unsplash.com/photo-1583324746266-6ad2266a2d89?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nzd8fGlsbHVzdHJhdGlvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+            alt="bird"
+          />
+        </figure>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
@@ -22,4 +180,8 @@
 export default {}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.draft {
+  height: 24rem;
+}
+</style>
