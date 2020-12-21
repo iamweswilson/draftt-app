fb517bd3d3b08eef5a941a78642e9c3ddb9c5dfc
Wes Wilson <wes@iamweswilson.com>
2020-12-21 11:36:31 -0800

    only calling uploadFile() if file exists

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,42 +1,54 @@
 <template>
   <div class="w-full">
     <h1 class="text-4xl text-blue-900 font-bold">Account Details</h1>
-    <form class="w-full sm:w-4/5 md:w-3/5" @submit="saveProfile">
+    <form class="w-full sm:w-4/5 md:w-3/5" @submit.prevent="saveProfile">
       <div class="mb-4">
         <div
             class="w-24 h-24 cursor-pointer rounded-full relative avatar"
             @click="$refs.file.click()"
         >
           <span
-            class="absolute w-8 h-8 rounded-full bg-blue-600 border-2 object-center p-1 border-white edit-icon"
+            class="absolute w-8 h-8 rounded-full bg-blue-600 border-2 object-center p-1 border-white z-10 edit-icon"
           >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white"
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="white"
           >
             <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
           </svg>
           </span>
+          <!-- Hidden img for preview -->
+          <img
+            class="h-full w-full rounded-full object-cover absolute z-0"
+            id="preview"
+          >
+            <!-- Show if user has profile img -->
           <img v-if="user.photoURL"
             :src="user.photoURL"
             alt="Avatar"
             class="h-full w-full rounded-full object-cover"
-            id="preview"
             >
+            <!-- Show if user has display name -->
           <div v-else-if="user.displayName"
             class="text-white uppercase text-4xl p-3 rounded-full bg-blue-600 w-full h-full flex items-center justify-center"
-            id="preview"
           >
-            {{ user.displayName[0] }}
+              {{ user.displayName[0] }}
           </div>
+          <!-- Show if they don't have any of the above -->
           <div v-else
             class="text-white uppercase text-4xl p-3 rounded-full bg-blue-600 w-full h-full flex items-center justify-center"
-            id="preview"
           >
             {{ user.email[0] }}
           </div>
-
         </div>
-        <input type="file" @change="chooseFile" class="hidden" ref="file">
       </div>
+      <input
+        type="file"
+        @change="chooseFile"
+        class="hidden"
+        ref="file"
+      >
       <div class="mb-4">
         <label class="block mb-2 text-gray-800 text-sm" for="name">Name</label>
         <input
@@ -77,6 +89,8 @@ import firebase from 'firebase'
 import { mapGetters, mapActions } from 'vuex'
 import UserDrafts from '~/components/user/UserDrafts.vue'
 
+var previewFile = '';
+
 export default {
   data() {
     return {
@@ -88,7 +102,10 @@ export default {
     }
   },
   computed: {
-    ...mapGetters({ user: 'user'})
+    ...mapGetters({ user: 'user'}),
+    fullFile: function () {
+      return this.file
+    }
   },
   components: {
     UserDrafts,
@@ -100,42 +117,44 @@ export default {
   methods: {
     ...mapActions(['updateUserName', 'updateUserEmail']),
     chooseFile() {
-      window.file = event.target.files[0];
+      window.file = event.target.files[0]
       // Show preview thumbnail
       var reader = new FileReader()
       reader.readAsDataURL(file)
       reader.onload = function(e) {
         document.getElementById('preview').src = e.target.result
       }
+      previewFile = file
+      console.log(previewFile)
     },
     uploadFile() {
-      var user = firebase.auth().currentUser;
-      console.log(file)
-      // Upload to firebase
-      firebase.storage().ref('users/' + user.uid + '/profilePic/' + 'avatar.jpg' ).put(file).then(res => {
-        console.log('upload worked')
+        if (previewFile !== '') {
+        console.log(file.name)
+          var user = firebase.auth().currentUser;
+          // Upload to firebase
+          firebase.storage().ref('users/' + user.uid + '/profilePic/' + 'avatar.jpg' ).put(file).then(res => {
+            console.log('upload worked')
 
-        // Get firebase url
-        var storageRef = firebase.storage().ref('users/' + user.uid + '/profilePic/' + 'avatar.jpg').getDownloadURL().then(imgURL => {
-          console.log('have download url')
-          // Update profile pic
-          user.updateProfile({
-            photoURL: imgURL
+            // Get firebase url
+            var storageRef = firebase.storage().ref('users/' + user.uid + '/profilePic/' + 'avatar.jpg').getDownloadURL().then(imgURL => {
+              console.log('have download url')
+              // Update profile pic
+              user.updateProfile({
+                photoURL: imgURL
+              })
+            })
+          }).catch(error => {
+            console.log(error.message);
           })
-        })
-      }).catch(error => {
-        console.log(error.message);
-      })
+        } else {
+          console.log('nope')
+        }
+
     },
     saveProfile() {
       this.updateUserName(this.displayName)
       this.updateUserEmail(this.email)
-      if ( this.file == '' ) {
-        console.log('no file')
-      } else {
-        this.uploadFile()
-        console.log('we has file')
-      }
+      this.uploadFile()
     },
     seeUser () {
       var user = firebase.auth().currentUser;
