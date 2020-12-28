6e17dd4cf9e29b821af9223d9ca6dfb1597556b8
Wes Wilson <wes@iamweswilson.com>
2020-12-27 19:09:19 -0800

    Starting upload form

--- pages/upload.vue
+++ pages/upload.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="container mx-auto">
     <h1 class="text-4xl text-blue-900 font-bold">Upload</h1>
-    <button 
+    <form>
+      <label for="title">Title</label>
+      <input
+        class="border w-full px-2 py-2 rounded-md"
+        type="text"
+        v-model="title"
+        @blur="createDraft(title, date)"
+      />
+      <button 
       type="submit"
-      @click="testUpload">Click me</button>
+      slot="reference"
+      >Click me</button>
+    </form>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase/app'
+const db = firebase.firestore();
+
 export default {
-  methods: {
-    testUpload() {
-      this.$store.dispatch('myModule/insert', {title: 'Hello Firestore 🔥'})
+  data() {
+    return {
+      title: '',
+      date: new Date().toISOString().slice(0, 10),
     }
+  },
+  methods: {
+    createDraft(title, date) {
+      if (title != "") {
+        console.log(title)
+        db.collection("drafts")
+          .add({ date: date, title: title })
+          .then(() => {
+            console.log("Document successfully written!");
+            // this.readEmployees();
+          })
+          .catch((error) => {
+            console.error("Error writing document: ", error);
+          });
+        this.title = ""
+      } else {
+        console.log('Give your draft a name')
+      }
+    },
   }
 }
 </script>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,6 +1,7 @@
 import firebase from '@firebase/app'
 import '@firebase/auth'
 import '@firebase/storage'
+import '@firebase/firestore'
 
 function initFirebase () {
   firebase.initializeApp({ 
